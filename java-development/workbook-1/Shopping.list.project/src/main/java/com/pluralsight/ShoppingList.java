c8491931a2265d30e21ab2ee3ce299a7ca9bd837
SpaceCowboyjojo <joseph.medina33@gmail.com>
2025-04-10 10:37:56 -0400

    Add files via upload

---
+++ java-development/workbook-1/Shopping.list.project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+import javax.swing.*;
+
+import static java.lang.System.*;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Milk");
+        System.out.println("Apples");
+        System.out.println("Juice");
+        System.out.println("Bread");
+        System.out.println("Water");
+        System.out.println("Oranges");
+        System.out.println("Ramen");
+        System.out.println("Utensils");
+        System.out.println("Steaks");
+        System.out.println("Chicken");
+
+    }
+}
